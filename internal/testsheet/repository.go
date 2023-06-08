3ffc77b2ab28df106fd8e0b67f6b963bf58bc62c
babulalt <babulal.tamang@berrybytes.com>
2023-06-08 22:02:27 +0545

    Comment enhance

--- internal/testsheet/repository.go
+++ internal/testsheet/repository.go
@@ -8,19 +8,19 @@ import (
 	"github.com/sugaml/qa-api/pkg/log"
 )
 
-// Repository encapsulates the logic to access albums from the data source.
+// Repository encapsulates the logic to access testsheets from the data source.
 type Repository interface {
-	// Get returns the album with the specified album ID.
+	// Get returns the testsheet with the specified testsheet ID.
 	Get(ctx context.Context, id string) (entity.TestSheet, error)
-	// Count returns the number of albums.
+	// Count returns the number of testsheets.
 	Count(ctx context.Context) (int, error)
-	// Query returns the list of albums with the given offset and limit.
+	// Query returns the list of testsheets with the given offset and limit.
 	Query(ctx context.Context, offset, limit int) ([]entity.TestSheet, error)
-	// Create saves a new album in the storage.
+	// Create saves a new testsheet in the storage.
 	Create(ctx context.Context, req entity.TestSheet) error
-	// Update updates the album with given ID in the storage.
+	// Update updates the testsheet with given ID in the storage.
 	Update(ctx context.Context, req entity.TestSheet) error
-	// Delete removes the album with given ID from the storage.
+	// Delete removes the testsheet with given ID from the storage.
 	Delete(ctx context.Context, id string) error
 }
 
@@ -29,30 +29,30 @@ type repository struct {
 	logger log.Logger
 }
 
-// NewRepository creates a new album repository
+// NewRepository creates a new testsheet repository
 func NewRepository(db *dbcontext.DB, logger log.Logger) Repository {
 	return repository{db, logger}
 }
 
-// Get reads the album with the specified ID from the database.
+// Get reads the testsheet with the specified ID from the database.
 func (r repository) Get(ctx context.Context, id string) (entity.TestSheet, error) {
 	var data entity.TestSheet
 
 	return data, nil
 }
 
-// Create saves a new album record in the database.
-// It returns the ID of the newly inserted album record.
+// Create saves a new testsheet record in the database.
+// It returns the ID of the newly inserted testsheet record.
 func (r repository) Create(ctx context.Context, req entity.TestSheet) error {
 	return nil
 }
 
-// Update saves the changes to an album in the database.
+// Update saves the changes to an testsheet in the database.
 func (r repository) Update(ctx context.Context, req entity.TestSheet) error {
 	return nil
 }
 
-// Delete deletes an album with the specified ID from the database.
+// Delete deletes an testsheet with the specified ID from the database.
 func (r repository) Delete(ctx context.Context, id string) error {
 	return nil
 }
